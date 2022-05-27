442b4966c79045b74d44a3a1b2ab4abf5d49775b
Abel Bokure <abel.ngt@gmail.com>
2022-05-27 14:24:27 -0400

    commit tree 54ad83a3c8003a13c2c132a00003e6019ac4e132

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}   
+function destructivelyPrependCat () {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat () {
+    cats.pop("Bob")
+}
+function destructivelyRemoveFirstCat () {
+    cats.shift("Milo")
+} 
+
+function appendCat () {
+    const allCats = [...cats, "Broom"]
+    return allCats
+}
+function prependCat () {
+    const moreCats = ["Arnold",...cats]
+    return moreCats
+}
+
+function removeLastCat () {
+    const catts = cats.slice(0,cats.length-1)
+    return catts
+}
+function removeFirstCat () {
+    const gato = cats.slice(1)
+    return gato
+}
